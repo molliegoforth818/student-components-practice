7b65371a2d4da45ec19d1accf6b5f683ba6c81ea
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-20 11:09:21 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const createStudentComponent = () => {
+    return `
+        <div class="student">
+            <h1>Alejandro Font</h1>
+            <section>Day cohort 27</section>
+            <aside>
+                Wore pants that were too short for his legs.
+                Was an incredible friend to his teammates.
+            </aside>
+        </div>
+    `
+}
